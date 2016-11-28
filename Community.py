7797a60e32d1346cb681255af7f937d6fbc1388d
fishticker <nishio.kishiro@gmail.com>
2016-11-28 18:23:48 +0100

    create community object

--- Community.py
+++ Community.py
@@ -1,1 +1,12 @@
 class Community:
+	def __init__(self, community = []):
+		_comunity = community
+
+	def getCommunity(self):
+		return self._comunity
+
+	def __getitem__(self,key):
+		return self._comunity[key]
+		
+	def __len__(self):
+		return len(self._comunity)
