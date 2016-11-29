4f16a223f01d8d89517527ca803ee0cdfecc0e79
fishticker <nishio.kishiro@gmail.com>
2016-11-29 16:44:02 +0100

    no real difference

--- Community.py
+++ Community.py
@@ -1,12 +1,21 @@
+from Node import *
+
+
 class Community:
 	def __init__(self, community = []):
-		_comunity = community
+		_community = community
+		_hubs = []
 
 	def getCommunity(self):
-		return self._comunity
+		return self._community
+
+	def setHubs(self):
+
+
+
 
 	def __getitem__(self,key):
-		return self._comunity[key]
-		
+		return self._community[key]
+
 	def __len__(self):
-		return len(self._comunity)
+		return len(self._community)
